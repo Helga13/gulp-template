6a5411cad45967767db1acf3e7bd853067e38bdf
Helga <black_twilight@list.ru>
2017-12-06 10:57:43 +0300

    Update app.js

--- prod/js/app.js
+++ prod/js/app.js
@@ -2,15 +2,4 @@ $(document).ready(function () {
   
   'use strict'
   
-//banner
-  
-  $('.js-slider').not('.slick-initialized').slick({
-    dots: true,
-    arrows: true,
-    infinite: true,
-    autoplay: true,
-    speed: 1000,
-    slidesToShow: 1
-  });
-  
 })
